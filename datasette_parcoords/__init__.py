8766d51c0762636c3f57ba530a21b3cd39d6cc68
koaning <vincentwarmerdam@gmail.com>
2021-04-14 19:22:51 +0200

    progress

--- datasette_parcoords/__init__.py
+++ datasette_parcoords/__init__.py
@@ -1,1 +1,28 @@
 from datasette import hookimpl
+import glob
+import os
+
+cache = {}
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+
+
+def cached_filepaths_for_extension(extension):
+    pattern = os.path.join(static_dir, "*.{}".format(extension))
+    if pattern not in cache:
+        cache[pattern] = [
+            "/-/static-plugins/datasette_parcoords/{}".format(os.path.basename(g))
+            for g in glob.glob(pattern)
+        ]
+    return cache[pattern]
+
+
+@hookimpl
+def extra_css_urls(view_name):
+    if view_name == "table":
+        return cached_filepaths_for_extension("css")
+
+
+@hookimpl
+def extra_js_urls(view_name):
+    if view_name == "table":
+        return cached_filepaths_for_extension("js")
--- datasette_parcoords/static/index_parcoords.js
+++ datasette_parcoords/static/index_parcoords.js
@@ -1,14 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
+window.onload = function(){
     const csvElem = document.querySelector('.export-links a[href*=csv]');
-    if (jsonEl) {
-        jsonUrl = jsonEl.getAttribute('href');
-        // Create elements for adding graph tool to page
-        visTool = document.createElement('div');
-        visTool.className = "parcoords"
-        visTool.id = "example-progressive"
+    // Create elements for adding graph tool to page
+    visTool = document.createElement('div');
+    visTool.className = "parcoords"
+    visTool.id = "parcoords-chart"
+
+    let table = document.querySelector('table.rows-and-columns');
+    table.parentNode.insertBefore(visTool, table);
+
+    let btn = document.createElement("button");
+    btn.textContent = "Parallel Coordinates"
+    btn.id = "parcoords-btn"
+    visTool.appendChild(btn)
+    document.getElementById("parcoords-btn").onclick = makechart;
+
+    var parcoords;
+    function makechart(){
+        d3.select("#parcoords-btn").remove();
+        d3.csv('/calmcode/smoking.csv', function(data) {
+            var colors = d3.scale.category20b();
+            // load csv file and create the chart
     
-        let table = document.querySelector('table.rows-and-columns');
-        table.parentNode.insertBefore(visTool, table);
+            var colorgen = d3.scale.ordinal()
+                .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
+                        "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
+                        "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
+    
+            // var color = function(d) { return colorgen(d.group) ? d.hasOwnProperty('group') : "#a6cee3"; };
+            var color = function(d) { return "#a6cee3" };
+            data.map(d => color(d))
+    
+            parcoords = d3.parcoords()("#parcoords-chart")
+                .data(data)
+                .hideAxis(["name"])
+                .color(color)
+                .alpha(0.55)
+                .composite("darken")
+                .margin({ top: 20, left: 10, bottom: 10, right: 0 })
+                .mode("queue")
+                .render()
+                .reorderable()
+                .brushMode("1D-axes");  // enable brushing
+    
+            parcoords.svg.selectAll("text").style("font", "10px sans-serif");
+        });
     }
-    
-});
+};
